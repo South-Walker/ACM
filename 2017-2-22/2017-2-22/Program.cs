6475f8b15f0b3458c565598eb9336b9f633162ad
Nanxing Xiao <506830915@qq.com>
2017-02-22 18:46:12 +0800

    try

---
+++ 2017-2-22/2017-2-22/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_2_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(add(10));
+            Console.Read();
+        }
+
+        static int add(int end)
+        {
+            if(end == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return add(end - 1) + end;
+            }
+        }//递归，累加
+    }
+}

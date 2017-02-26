0ed77670d3a88f5c3c237e8b3ba2ca6c962e63c7
Nanxing Xiao <506830915@qq.com>
2017-02-26 20:36:24 +0800

    2017-2-26

--- 2017-2-25/2017-2-25/Program.cs
+++ 2017-2-25/2017-2-25/Program.cs
@@ -10,7 +10,12 @@ namespace _2017_2_25
     {
         static void Main(string[] args)
         {
-
+            Linked link = new Linked();
+            for (int i = 0; i < 100; i++)
+            {
+                link.Insert(i, i);
+            }
+            Console.Read();
         }
     }
     class Node//构造节点类
@@ -35,10 +40,21 @@ namespace _2017_2_25
         {
             header = new Node("header");
         }
-        public Node Find(object obj)
+        public Node Find(object obj,Boolean is_return_the_end)
         {
             Node temp = header;
-            while(temp.link != null||temp.content != obj)
+            while (temp.link != null && temp.content != obj) 
+            {
+                temp = temp.link;
+            }
+            if (is_return_the_end || temp.content == obj)
+                return temp;
+            else return new Node();
+        }
+        public Node Find_Previous(object obj)
+        {
+            Node temp = header;
+            while (temp.link != null && temp.link.content != obj)
             {
                 temp = temp.link;
             }
@@ -47,13 +63,15 @@ namespace _2017_2_25
         public void Insert(object acontent,object after)
         {
             Node new_node = new Node(acontent);
-            Node temp = Find(after);
+            Node temp = Find(after,true);
             new_node.link = temp.link;
             temp.link = new_node;
         }
         public void Remove(object thecontent)
         {
-            Node temp = Find(thecontent);
+            Node temp = Find_Previous(thecontent);
+            if (temp.link != null)
+                temp.link = temp.link.link;
         }
     }
 }
---
+++ 2017-2-26/2017-2-26/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_2_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkOfQuestion test = new LinkOfQuestion(41);
+            test.Remove(test.FindLast());
+            Console.Read();
+        }
+    }
+    class Node
+    {
+        public Node link;
+        public object obj = null;
+        public Node()
+        {
+            link = null;
+        }
+        public Node(object thisobj)
+        {
+            obj = thisobj;
+            link = null;
+        }
+    }
+    class Linked
+    {
+        protected Node header;
+        protected int count;
+        public Linked()
+        {
+            header = new Node("header");
+            header.link = header;
+            count = 0;
+        }
+        public int Find(object content)
+        {
+            if(count == 0)
+            {
+                return 0;
+            }
+            Node temp = header;int nownum = 0;
+            do
+            {
+                if (temp.obj == content)
+                {
+                    return nownum;
+                }
+                temp = temp.link;
+                nownum++;
+            } while (temp != header);
+            return -1;
+        }
+        public int Insert(object obj,object after)
+        {
+            int num = Find(after);
+            Node aim = new Node(obj);
+            if(num < 0)
+            {
+                return -1;
+            }
+            Node temp = header;
+            for (int i = 0; i < num; i++) 
+            {
+                temp = temp.link;
+            }
+            aim.link = temp.link;
+            temp.link = aim;
+            count++;
+            return 1;
+        }
+    }
+    class LinkOfQuestion
+    {
+        public Node first = new Node(1);
+        protected int count = 1;
+        public LinkOfQuestion(int person)
+        {
+            first.link = first;
+            for (int i = 2; i <= person; i++)
+            {
+                Node last = FindLast();
+                Node aim = new Node(i);
+                aim.link = first;
+                last.link = aim;
+                count++;
+            }
+        }
+        public Node FindLast()
+        {
+            Node now = first;
+            for (int i = 1; i < count; i++)
+            {
+                now = now.link;
+            }
+            return now;
+        }
+        public void Remove(Node previous)
+        {
+            Node itself = previous.link;
+            previous.link = itself.link;
+            count--;
+            Console.WriteLine(itself.obj.ToString());
+            first = itself;
+            while (count >= 3)
+                KillSelf(first);
+        }
+        public void KillSelf(Node begin)
+        {
+            Node previous = begin.link.link.link;
+            Remove(previous);
+        }
+    }
+}

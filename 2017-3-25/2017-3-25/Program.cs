f99f655b83d7cdef07a2ab366fac400d822fc2de
Nanxing Xiao <506830915@qq.com>
2017-03-27 18:31:00 +0800

    2017-3-25

--- 2017-3-25/2017-3-25/Program.cs
+++ 2017-3-25/2017-3-25/Program.cs
@@ -9,22 +9,47 @@ namespace _2017_3_25
 {
     class Program
     {
-        static int[] inorder = { 3, 2, 1, 4, 5, 7, 6 };
-        static int[] postorder = { 3, 1, 2, 5, 6, 7, 4 };
+        static int[] inorder = { 7, 8, 11, 3, 5, 16, 12, 18 };
+        static int[] postorder = { 8, 3, 11, 7, 16, 18, 12, 5 };
         static void Main(string[] args)
         {
+            DateTime t1 = DateTime.Now;
             uva548();
+            DateTime t2 = DateTime.Now;
+            TimeSpan ts = t2.Subtract(t1);
+            Console.WriteLine(ts.Milliseconds);
             Console.Read();
         }
         public static void uva548()
         {
             Node root = new Node(postorder[postorder.Length - 1]);
             BuildTree(0, inorder.Length - 1, 0, postorder.Length - 2, root);
-            Console.Read();
+            int max = 999999;
+            int now = 0;
+            int answer = 0;
+            CheckSum(ref now, root, ref max,ref answer);
+            Console.WriteLine(answer);
+        }
+        public static void CheckSum(ref int now, Node nowworking, ref int max, ref int answer)
+        {
+            now += nowworking.value;
+            if (now < max) 
+            {
+                if (nowworking.left == null && nowworking.right == null)
+                {
+                    max = now;
+                    answer = nowworking.value;
+                }
+                if (nowworking.left != null)
+                    CheckSum(ref now, nowworking.left, ref max, ref answer);
+                if (nowworking.right != null)
+                    CheckSum(ref now, nowworking.right, ref max, ref answer);
+            }
+            now -= nowworking.value;
         }
         public static void BuildTree(int inorderbegin, int inorderend, int postorderbegin, int postorderend, Node root)
         {
-            if (inorderbegin < inorderend) 
+            if (inorderbegin < inorderend)
             {
                 int inorderindex = 0;
                 for (int i = inorderbegin; i <= inorderend; i++)
@@ -35,11 +60,18 @@ namespace _2017_3_25
                         break;
                     }
                 }
-                int count = inorderindex - inorderbegin;
-                root.right = new Node(postorder[postorderend]);
-                root.left = new Node(postorder[inorderbegin + count - 1]);
-                BuildTree(inorderbegin, inorderindex - 1, postorderbegin, inorderbegin + count - 2, root.left);
-                BuildTree(inorderindex + 1, inorderend, inorderbegin + count - 1, postorderend - 1, root.right);
+                int leftcount = inorderindex - inorderbegin;
+                int rightcount = inorderend - inorderindex;
+                if (rightcount != 0)
+                {
+                    root.right = new Node(postorder[postorderend]);
+                }
+                if (leftcount != 0)
+                {
+                    root.left = new Node(postorder[postorderbegin + leftcount - 1]);
+                }
+                BuildTree(inorderbegin, inorderindex - 1, postorderbegin, inorderbegin + leftcount - 2, root.left);
+                BuildTree(inorderindex+1, inorderend, inorderbegin + leftcount, postorderend - 1, root.right);
             }
             else { }
         }   

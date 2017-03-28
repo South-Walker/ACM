fe913cbc524d7d65af54a18c1463e443ef250913
Nanxing Xiao <506830915@qq.com>
2017-03-28 22:24:01 +0800

    2017-3-28

--- 2017-3-28/2017-3-28/Program.cs
+++ 2017-3-28/2017-3-28/Program.cs
@@ -10,6 +10,40 @@ namespace _2017_3_28
     {
         static void Main(string[] args)
         {
+            DateTime t1 = DateTime.Now;
+            uva699();
+            DateTime t2 = DateTime.Now;
+            TimeSpan ts = t2.Subtract(t1);
+            Console.WriteLine(ts.Milliseconds);
+            Console.ReadLine();
+        }
+        public static void uva699()
+        {
+            int[] aim = new int[10000];
+            int now = 5000;
+            int stindex = -1;
+            string input = "8 2 9 -1 -1 6 5 -1 -1 12 -1 -1 3 7 -1 -1 -1";
+            string[] sta = input.Split(' ');
+            Sum(aim, now, ref stindex, sta);
+            for (int i = 0; i < 10000; i++)
+            {
+                if(aim[i] == 0)
+                {
+                    continue;
+                }
+                Console.Write(aim[i] + " ");
+            }
+            Console.WriteLine();
+        }
+        public static void Sum(int[] aim, int now, ref int index, string[] sta)
+        {
+            index += 1;
+            if (sta[index] != "-1")
+            {
+                aim[now] += Convert.ToInt32(sta[index]);
+                Sum(aim, now - 1, ref index, sta);
+                Sum(aim, now + 1, ref index, sta);
+            }
         }
     }
 }

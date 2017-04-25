4cfb77ddc52f1f34651f00436767fc0eae84577e
Nanxing Xiao <506830915@qq.com>
2017-04-25 11:57:07 +0800

    2017-4-25

---
+++ 2017-4-24/2017-4-24/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_4_24
+{
+    class Program
+    {
+        static int[,] arr;
+        static void Main(string[] args)
+        {
+            Risk();
+        }
+        public static void Risk()
+        {
+            arr = new int[21, 21];
+            FillArr(1, 3);
+            FillArr(2, 3, 4);
+            FillArr(3, 4, 5, 6);
+            FillArr(4, 6);
+            FillArr(5, 7);
+            FillArr(6, 12, 13);
+            FillArr(7, 8);
+            FillArr(8, 9, 10);
+            FillArr(9, 11);
+            FillArr(10, 11);
+            FillArr(11, 12, 17);
+            FillArr(12, 14);
+            FillArr(13, 14, 15);
+            FillArr(14, 15, 16);
+            FillArr(15, 16);
+            FillArr(16, 19);
+            FillArr(17, 18, 19);
+            FillArr(18, 20);
+            FillArr(19, 20);
+            for (int a = 1; a <= 20; a++)
+            {
+                for (int b = 1; b <= 20; b++)
+                {
+                    if (arr[a, b] != 1)
+                        arr[a, b] = 20;
+                }
+            }
+            for (int a = 1; a <= 20; a++)
+            {
+                for (int b = 1; b <= 20; b++)
+                {
+                    for (int c = 1; c <= 20; c++)
+                    {
+                        if (arr[c, b] > arr[c, a] + arr[a, b])
+                            arr[c, b] = arr[b, c] = arr[c, a] + arr[a, b];
+                    }
+                }
+            }
+            Console.Read();
+        }
+        public static void FillArr(int now, params int[] input)
+        {
+            foreach (int a in input)
+            {
+                arr[now, a] = 1;
+                arr[a, now] = 1;
+            }
+        }
+    }
+}
---
+++ 2017-4-25/2017-4-25/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_4_25
+{
+    class Program
+    {
+        static int[,] arr;
+        static void Main(string[] args)
+        {
+            Guide();
+        }
+        public static void Guide()
+        {
+            arr = new int[8, 8];
+            FillArr(1, 2, 30);
+            FillArr(1, 3, 15);
+            FillArr(1, 4, 10);
+            FillArr(2, 4, 25);
+            FillArr(2, 5, 60);
+            FillArr(3, 4, 40);
+            FillArr(3, 6, 20);
+            FillArr(4, 7, 35);
+            FillArr(5, 7, 20);
+            FillArr(6, 7, 30);
+            int passenger = 99;
+            int scity = 1;
+            int dcity = 7;
+            for (int a = 1; a < 8; a++)
+            {
+                for (int b = 1; b < 8; b++)
+                {
+                    for (int c = 1; c < 8; c++)
+                    {
+                        if (arr[b, c] < min(arr[a, b], arr[c, a]))
+                        {
+                            arr[b, c] = arr[c, b] = min(arr[a, b], arr[c, a]);
+                        }
+                    }
+                }
+            }
+            Console.Read();
+        }
+        public static void FillArr(int index1, int index2, int maxnum)
+        {
+            arr[index1, index2] = arr[index2, index1] = maxnum - 1;
+        }
+        public static int min(params int[]num)
+        {
+            int min = num[0];
+            foreach (int a in num)
+            {
+                if (a < min)
+                {
+                    min = a;
+                }
+            }
+            return min;
+        }
+    }
+}

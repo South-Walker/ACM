62426a4577d16a080c69cee2ae7f3c11f1f7c7f1
Nanxing Xiao <506830915@qq.com>
2017-05-31 15:51:07 +0800

    2017-5-31

---
+++ 2017-5-30/2017-5-30/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_5_30
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+        
+    }
+    class Program
+    {
+        public static ListNode AddTwo(ListNode l1, ListNode l2)
+        {
+            bool is_full = false; ListNode temp;
+            int now1 = l1.val;int now2 = l2.val;
+            int add = now1 + now2;
+            if (add >= 10)
+            {
+                add -= 10;
+                is_full = true;
+            }
+            ListNode l3 = new ListNode(add);
+            temp = l3;
+            ListNode temp1 = l1.next;
+            ListNode temp2 = l2.next;
+            while (temp1 != null && temp2 != null)
+            {
+                now1 = temp1.val;
+                now2 = temp2.val;
+                temp1 = temp1.next;
+                temp2 = temp2.next;
+                if (is_full)
+                {
+                    now1++;
+                    is_full = false;
+                }
+                add = now1 + now2;
+                if (add >= 10)
+                {
+                    add -= 10;
+                    is_full = true;
+                }
+                temp.next = new ListNode(add);
+                temp = temp.next;
+            }
+            while (temp2 != null)
+            {
+                now2 = temp2.val;
+                temp2 = temp2.next;
+                if (is_full)
+                {
+                    is_full = false;
+                    now2++;
+                }
+                if (now2 >= 10)
+                {
+                    is_full = true;
+                    now2 -= 10;
+                }
+                temp.next = new ListNode(now2);
+                temp = temp.next;
+            }
+            while (temp1 != null)
+            {
+                now1 = temp1.val;
+                temp1 = temp1.next;
+                if (is_full)
+                {
+                    is_full = false;
+                    now1++;
+                }
+                if (now1 >= 10)
+                {
+                    is_full = true;
+                    now1 -= 10;
+                }
+                temp.next = new ListNode(now1);
+                temp = temp.next;
+            }
+            if (is_full)
+            {
+                temp.next = new ListNode(1);
+            }
+            return l3;
+        }
+        public static int LengthOfLongestSubstring(string s)
+        {
+            int max = 1;int len = s.Length;string temp;bool is_exist = false;
+            List<string> little = new List<string>();
+
+            for (int i = 0; i < len; i++)
+            {
+                temp = s.Substring(i, 1);
+                foreach (string a in little)
+                {
+                    if (a == temp)
+                    {
+                        is_exist = true;
+                        break;
+                    }
+                }
+                if (!is_exist)
+                    little.Add(temp);
+                is_exist = false;
+            }
+            max = little.Count;
+            if (max == 1)
+                return max;
+
+            int now_max = 0;
+            little = new List<string>();
+            for (int i = 0; i < len; i++)
+            {
+                temp = s.Substring(i, 1);
+                for (int k = 0; k < little.Count; k++)
+                {
+                    if (little[k] == temp)
+                    {
+                        now_max = (now_max > little.Count) ? now_max : little.Count;
+                        for (int j = 0; j <= k; j++)
+                        {
+                            little.RemoveAt(0);
+                        }
+                        little.Add(temp);
+                        break;
+                    }
+                    if (k == little.Count - 1) 
+                    {
+                        little.Add(temp);
+                        break;
+                    }
+                }
+                if (little.Count == 0)
+                    little.Add(temp);
+                if (now_max == max)
+                    break;
+            }
+            now_max = (now_max > little.Count) ? now_max : little.Count;
+            return now_max;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(LengthOfLongestSubstring("dvdf"));
+            Console.Read();
+        }
+    }
+}
---
+++ 2017-5-31/2017-5-31/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_5_31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LengthOfLongestSubstring("bb");
+        }
+        public static string Convert2(string s, int numRows)
+        {
+            if (s == "")
+                return "";
+            if (numRows == 1)
+                return s;
+            string temp = "";
+            char[] array = s.ToCharArray();
+            int len = s.Length;int n;int num = 0;
+            for (int i = 1; i <= numRows; i++)
+            {
+                num = 0;
+                n = 2 * numRows * num - 2 * num + i;
+                while (n <= len)
+                {
+                    temp += array[n - 1];
+                    num++;
+                    if (i != 1 && i != numRows) 
+                    {
+                        n = 2 * numRows * num - 2 * num - i + 2;
+                        if (n <= len)
+                            temp += array[n - 1];
+                    }
+                    n = 2 * numRows * num - 2 * num + i;
+                }
+            }
+            return temp;
+        }
+        public static string Convert(string s, int numRows)
+        {
+            if (s == "")
+                return "";
+            if (numRows == 1)
+                return s;
+            Queue<string>[] arraylist = new Queue<string>[numRows];
+            for (int i = 0; i < arraylist.Length; i++) 
+            {
+                arraylist[i] = new Queue<string>();
+            }
+            int len = s.Length;string temp;
+            int now = 0;int index;
+            while(true)
+            {
+                temp = s.Substring(now, 1);
+                index = now % (2 * numRows - 2);
+                if (index < numRows - 1) 
+                {
+                    arraylist[index].Enqueue(temp);
+                }
+                else
+                {
+                    index = 2 * numRows - 2 - index;
+                    arraylist[index].Enqueue(temp);
+                }
+                now++;
+                if (now == len)
+                    break;
+            }
+            temp = "";
+            for (int i = 0; i < arraylist.Length; i++)
+            {
+                Queue<string> nowq = arraylist[i];
+                while (nowq.Count != 0)
+                {
+                    temp = temp + nowq.Dequeue();
+                }
+            }
+            return temp;
+        }
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0)
+                return false;
+            double y = x;
+            double z = 0;
+            while (true)
+            {
+                z = z * 10 + x % 10;
+                x = x / 10;
+                if (x == 0)
+                    break;
+            }
+            return (z == y) ? true : false;
+        }
+        public static int LengthOfLongestSubstring(string s)
+        {
+            Dictionary<char, bool> hashtable = new Dictionary<char, bool>();
+
+            int end = 0; char temp; int nowmax = 0; char now; int max = 0;int len = s.Length;int begin = 0;char[] array = s.ToCharArray();
+            for (; end < len; end++) 
+            {
+                now = array[end];
+                if (hashtable.ContainsKey(now) && hashtable[now]) 
+                {
+                    nowmax = end - begin;
+                    if (nowmax > max)
+                        max = nowmax;
+                    nowmax = 0;
+                    for (; begin < end; begin++)
+                    {
+                        temp = array[begin];
+                        if (temp == now)
+                        {
+                            begin++;
+                            break;
+                        }
+                        else
+                        {
+                            hashtable[temp] = false;
+                        }
+                    }
+                }
+                else
+                {
+                    hashtable[now] = true;
+                }
+            }
+            nowmax = end - begin;
+            if (nowmax > max)
+                max = nowmax;
+            return max;
+        }
+    }
+}

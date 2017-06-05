0a2ca0faa9313afe9d9ee615405169dce3b1a9ea
Nanxing Xiao <506830915@qq.com>
2017-06-05 21:11:36 +0800

    2017-6-5

---
+++ 2017-6-3/2017-6-3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_6_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = { "a", "aa" };
+            Console.WriteLine(LongestCommonPrefix(input));
+            Console.Read();
+        }
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            if (strs.Length == 0)
+                return "";
+            char[] now;string result = "";char[] commonsubstring;int min = 999999;int index = 0;
+            for (int i = 0; i < strs.Length; i++)
+            {
+                if (strs[i].Length < min) 
+                {
+                    min = strs[i].Length;
+                    index = i;
+                    if (min == 0)
+                        return "";
+                }
+            }
+            commonsubstring = strs[index].ToCharArray();
+            for (int i = 0; i < strs.Length; i++)
+            {
+                now = strs[i].ToCharArray();
+                bool iscommon = true;
+                for (int k = 0; k < commonsubstring.Length; k++)
+                {
+                    if (iscommon && now[k] == commonsubstring[k]) 
+                    {
+                        continue;
+                    }
+                    iscommon = false;
+                    commonsubstring[k] = '$';
+                }
+            }
+            for (int i = 0; i < commonsubstring.Length; i++)
+            {
+                if (commonsubstring[i] != '$')
+                    result = result + commonsubstring[i];
+            }
+            return result;
+        }
+    }
+}
---
+++ 2017-6-5/2017-6-5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_6_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MaxArea(1,2);
+            
+        }
+
+        public static int MaxArea(params int[] height)
+        {
+            int result = 0;int begin = 0;int end = height.Length - 1;
+            int now;
+            while (end > begin) 
+            {
+                now = end - begin;
+                if (height[end] > height[begin])
+                {
+                    now = now * height[begin];
+                    begin++;
+                }
+                else
+                {
+                    now = now * height[end];
+                    end--;
+                }
+                if (now > result) 
+                {
+                    result = now;
+                }
+            }
+            return result;
+        }
+    }
+}

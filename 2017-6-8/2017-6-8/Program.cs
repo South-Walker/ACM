9d4c84db4285401c7c6b5ddb1079a00463697549
Nanxing Xiao <506830915@qq.com>
2017-06-09 21:18:10 +0800

    2017-6-9

---
+++ 2017-6-8/2017-6-8/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_6_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program p1 = new Program();
+            Console.Write(p1.isMatch("a", "ab*"));
+            Console.Read();
+        }
+        public bool MYIsMatch(string s, string p)
+        {
+            Check(s.ToCharArray(), p.ToCharArray(), 0, 0);
+            return temp;
+        }
+        bool temp = false;
+        public void Check(char[] cs, char[] cp, int indexs, int indexp)
+        {
+            if (indexs >= cs.Length && indexp >= cp.Length)
+            {
+                temp = true;
+                return;
+            }
+            char after = '$';char nows;char nowp;
+            while (indexp < cp.Length)
+            {
+                nows = (indexs >= cs.Length) ? '$' : cs[indexs];
+                nowp = cp[indexp];
+                if (indexp < cp.Length - 1)
+                {
+                    after = cp[indexp + 1];
+                }
+                if (after != '*')
+                {
+                    if (nows == '$')
+                        return;
+                    if (nowp == '.' || nowp == nows)
+                    {
+                        indexp++; indexs++;
+                    }
+                    else
+                    {
+                        return;
+                    }
+                }
+                else//if(after=='*')
+                {
+                    Check(cs, cp, indexs, indexp + 2);
+                    while (indexs < cs.Length) 
+                    {
+                        nows = cs[indexs];
+                        if (nowp == '.' || nows == nowp) 
+                        {
+                            Check(cs, cp, indexs + 1, indexp + 2);
+                        }
+                        else
+                            break;
+                        indexs++;
+                    }
+                    break;
+                }
+            }
+            if (indexs >= cs.Length && indexp >= cp.Length)
+            {
+                temp = true;
+                return;
+            }
+        }
+        public bool isMatch(String s, String p)
+        {
+            if (s.Length == 0 && p.Length == 0)
+                return true;
+            if (p.Length == 0)
+                return false;
+            bool[,] res = new bool[s.Length + 1,p.Length + 1];
+            res[0,0] = true;
+            for (int j = 0; j < p.Length; j++)
+            {
+                if (p[j] == '*')
+                {
+                    if (j > 0 && res[0,j - 1]) res[0,j + 1] = true;
+                    if (j < 1) continue;
+                    if (p[j - 1] != '.')
+                    {
+                        for (int i = 0; i < s.Length; i++)
+                        {
+                            if (res[i + 1,j] || j > 0 && res[i + 1,j - 1]
+                            || i > 0 && j > 0 && res[i,j + 1] && s[i] == s[i - 1] && s[i - 1] == p[j - 1])
+                                res[i + 1,j + 1] = true;
+                        }
+                    }
+                    else
+                    {
+                        int i = 0;
+                        while (j > 0 && i < s.Length && !res[i + 1,j - 1] && !res[i + 1,j])
+                            i++;
+                        for (; i < s.Length; i++)
+                        {
+                            res[i + 1,j + 1] = true;
+                        }
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        if (s[i] == p[j] || p[j] == '.')
+                            res[i + 1,j + 1] = res[i,j];
+                    }
+                }
+            }
+            return res[s.Length, p.Length];
+        }
+    }
+}
---
+++ 2017-6-9/2017-6-9/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_6_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program a = new Program();
+            Console.Write(a.IsValid("()[]{}"));
+            Console.Read();
+        }
+        public bool IsValid(string s)
+        {
+            Stack<char> check = new Stack<char>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                char now = s[i];
+                switch (now)
+                {
+                    case '(':
+                        check.Push(now);
+                        break;
+                    case '{':
+                        check.Push(now);
+                        break;
+                    case '[':
+                        check.Push(now);
+                        break;
+                    case ')':
+                        if (check.Count == 0)
+                            return false;
+                        if (check.Pop() != '(')
+                            return false;
+                        break;
+                    case '}':
+                        if (check.Count == 0)
+                            return false;
+                        if (check.Pop() != '{')
+                            return false;
+                        break;
+                    case ']':
+                        if (check.Count == 0)
+                            return false;
+                        if (check.Pop() != '[')
+                            return false;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            if (check.Count != 0)
+                return false;
+            return true;
+        }
+        public static ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null)
+                return l2;
+            if (l2 == null)
+                return l1;
+            ListNode head = new ListNode(0);
+            ListNode now = head;
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val > l2.val)
+                {
+                    now.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    now.next = l2;
+                    l2 = l2.next;
+                }
+                now = now.next;
+            }
+            if (l1 == null)
+            {
+                while (l2 != null)
+                {
+                    now.next = l2;
+                    l2 = l2.next;
+                }
+            }
+            else
+            {
+                while (l1 != null)
+                {
+                    now.next = l1;
+                    l1 = l1.next;
+                }
+            }
+            return head.next;
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+}

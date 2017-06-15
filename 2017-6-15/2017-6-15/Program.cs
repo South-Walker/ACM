d63f10e486635ad8b0712e480576ba1662b4cd23
Nanxing Xiao <506830915@qq.com>
2017-06-15 16:38:13 +0800

    2017-6-15

---
+++ 2017-6-15/2017-6-15/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_6_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        public int RemoveElement(int[] nums, int val)
+        {
+            int tail = nums.Length - 1;
+            if (tail < 0)
+                return 0;
+            int now = 0;
+            while (now != tail)
+            {
+                if (nums[now] == val)
+                {
+                    nums[now] = nums[tail];
+                    tail--;
+                }
+                else
+                {
+                    now++;
+                }
+            }
+            if (nums[now] != val)
+                return now + 1;
+            else
+                return now;
+        }
+    }
+}

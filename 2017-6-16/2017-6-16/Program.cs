f722400c86e9c99c3b90b0db657a8671d9ec2444
Nanxing Xiao <506830915@qq.com>
2017-06-17 20:37:35 +0800

    2017-6-17

--- 2017-6-16/2017-6-16/Program.cs
+++ 2017-6-16/2017-6-16/Program.cs
@@ -10,9 +10,118 @@ namespace _2017_6_16
     {
         static void Main(string[] args)
         {
+            int[] nums = { 1, 0, 0, -1, 2, -2 };
             Program a = new Program();
-            a.LetterCombinations("23");
+            a.FourSum(nums, 0);
         }
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            qsort(nums, 0, nums.Length - 1);
+            IList<IList<int>> result = new List<IList<int>>();
+            IList<int> temp = new List<int>();
+            if (nums.Length == 0)
+                return result;
+            int beginindex;int endindex;
+            for (int a = 0; a <= nums.Length - 4; a++)
+            {
+                for (int b = a + 1; b <= nums.Length - 3; b++) 
+                {
+                    beginindex = b + 1;
+                    endindex = nums.Length - 1;
+                    while (beginindex < endindex)
+                    {
+                        if (nums[a] + nums[b] + nums[beginindex] + nums[endindex] > target)
+                        {
+                            int endnow = nums[endindex];
+                            while (beginindex < endindex)
+                            {
+                                if (endnow == nums[endindex])
+                                    endindex--;
+                                else
+                                    break;
+                            }
+                        } 
+                        else if(nums[a] + nums[b] + nums[beginindex] + nums[endindex] < target)
+                        {
+                            int beginnow = nums[beginindex];
+                            while(beginindex<endindex)
+                            {
+                                if (beginnow == nums[beginindex])
+                                    beginindex++;
+                                else
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            temp.Add(nums[a]);temp.Add(nums[b]);temp.Add(nums[beginindex]);temp.Add(nums[endindex]);
+                            result.Add(temp);
+                            temp = new List<int>();
+                            int endnow = nums[endindex];
+                            while (beginindex < endindex)
+                            {
+                                if (endnow == nums[endindex])
+                                    endindex--;
+                                else
+                                    break;
+                            }
+                            int beginnow = nums[beginindex];
+                            while (beginindex < endindex)
+                            {
+                                if (beginnow == nums[beginindex])
+                                    beginindex++;
+                                else
+                                    break;
+                            }
+                        }
+                    }
+
+                    int testb = nums[b + 1];
+                    while (testb == nums[b])
+                    {
+                        b++;
+                        if (b >= nums.Length - 3)
+                            break;
+                        testb = nums[b + 1];
+                    }
+                }
+                int testa = nums[a + 1];
+                while (testa == nums[a])
+                {
+                    a++;
+                    if (a >= nums.Length - 4)
+                        break;
+                    testa = nums[a + 1];
+                }
+            }
+
+
+            return result;
+        }
+        public void qsort(int[] nums, int begin, int end)
+        {
+            if (begin < end)
+            {
+                int b = begin; int e = end; int temp;
+                while (b < e)
+                {
+                    while (b < e && nums[b] <= nums[e])
+                    {
+                        b++;
+                    }
+                    temp = nums[b]; nums[b] = nums[e]; nums[e] = temp;
+                    while (b < e && nums[e] >= nums[b])
+                    {
+                        e--;
+                    }
+                    temp = nums[b]; nums[b] = nums[e]; nums[e] = temp;
+                }
+                qsort(nums, begin, b - 1);
+                qsort(nums, e + 1, end);
+            }
+        }
+
+
         static string[][] table = new string[10][];
         public IList<string> LetterCombinations(string digits)
         {
---
+++ 2017-6-17/2017-6-17/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_6_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+       /*     head.next.next = new ListNode(3);
+            head.next.next.next = new ListNode(4);
+            head.next.next.next.next = new ListNode(5);*/
+            Solution a = new Solution();
+            a.RemoveNthFromEnd(head, 1);
+        }
+    }
+  public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) { val = x; }
+  }
+    public class Solution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (head.next == null && n >= 1) 
+            {
+                return null;
+            }
+            ListNode tail = head;
+            ListNode mid = head;
+            for (int i = 0; i < n; i++)
+            {
+                tail = tail.next;
+            }
+            if (tail == null)
+            {
+                return head.next;
+            }
+            else
+            {
+                while (tail.next != null)
+                {
+                    tail = tail.next;
+                    mid = mid.next;
+                }
+            }
+            mid.next = mid.next.next;
+            return head;
+        }
+    }
+}

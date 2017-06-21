eee868a861169ea79023b01ce02631e7f7cdf420
Nanxing Xiao <506830915@qq.com>
2017-06-21 22:13:58 +0800

    2017-6-21

--- 2017-6-20/2017-6-20/Program.cs
+++ 2017-6-20/2017-6-20/Program.cs
@@ -10,6 +10,77 @@ namespace _2017_6_20
     {
         static void Main(string[] args)
         {
+            Solution a = new Solution();
+            Console.WriteLine(a.Divide(-2147483648, -1109186033));
+            Console.Read();
+        }
+    }
+    public class Solution
+    {
+        public static bool Isminus(int dividend, int divisor)
+        {
+            bool ispositive = (dividend >> 31) - (divisor >> 31) == 0;
+            return ispositive;
+        }
+        public int Divide(int dividend, int divisor)
+        {
+            int result = 0;
+            if (divisor == 0)
+                return int.MaxValue;
+            bool ispositive = Isminus(dividend, divisor);
+            if (dividend == int.MinValue)
+            {
+                if (divisor != int.MinValue)
+                {
+                    if (divisor == -1)
+                    {
+                        return int.MaxValue;
+                    }
+                    if (ispositive)
+                    {
+                        dividend = dividend + divisor;
+                        result++;
+                    }
+                    else
+                    {
+                        dividend = dividend - divisor;
+                        result++;
+                    }
+                }
+                else
+                {
+                    return 1;
+                }
+            }
+            if (divisor == int.MinValue)
+            {
+                return 0;
+            }
+            dividend = Math.Abs(dividend);
+            divisor = Math.Abs(divisor);
+            while (dividend > divisor) 
+            {
+                int has_righttimes = 0;int divisornow = divisor;
+                while (dividend > divisornow && divisornow < 1073741824) 
+                {
+                    has_righttimes += 1;
+                    divisornow = divisornow << 1;
+                }
+                if (dividend <= divisornow)
+                {
+                    divisornow = divisornow >> 1;
+                    has_righttimes--;
+                }
+                dividend = dividend - divisornow;
+                result += 1 << has_righttimes;
+            }
+            if (divisor == dividend)
+                result++;
+            if (!ispositive)
+            {
+                result *= -1;
+            }
+            return result;
         }
     }
 }
---
+++ 2017-6-21/2017-6-21/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_6_21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution a = new Solution();
+            a.CountAndSay(5);
+        }
+    }
+    public class Solution
+    {
+        static List<string> table = new List<string>();
+        public string CountAndSay(int n)
+        {
+            if (table.Count == 0)
+            {
+                table.Add("1");
+            }
+            while (table.Count < n)
+            {
+                table.Add(getnext());
+            }
+            return table[n - 1];
+        }
+        public string getnext()
+        {
+            string now = table[table.Count - 1];
+            string next = "";char temp = now[0];int times = 1;
+            for (int i = 1; i < now.Length; i++) 
+            {
+                if (temp == now[i])
+                {
+                    times++;
+                }
+                else
+                {
+                    next = next + times + temp;
+                    times = 1;
+                    temp = now[i];
+                }
+            }
+            next = next + times + temp;
+            return next;
+        }
+    } 
+}

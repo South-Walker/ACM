6811aae96aae38556cf937bae58917769da7965d
Nanxing Xiao <506830915@qq.com>
2017-09-19 13:10:58 +0800

    2017-9-19

---
+++ 2017-9-19/2017-9-19/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_9_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = new int[] { 2, 3, 6, 7 };
+            Solution a = new Solution();
+            a.CombinationSum(test, 7);
+            
+        }
+    }
+    public class Solution
+    {
+        public IList<IList<int>> CombinationSum(int[] candidates, int target)
+        {
+            IList<IList<int>> result = new List<IList<int>>();
+            retell(candidates, target, result, new List<int>(), candidates.Length - 1);
+            return result;
+        }
+        public IList<int> deepcopy(IList<int> aim)
+        {
+            List<int> newlist = new List<int>();
+            for (int i = 0; i < aim.Count; i++) 
+            {
+                newlist.Add(aim[i]);
+            }
+            return newlist;
+        }
+        public void retell(int[] candidates, int target, IList<IList<int>> result, IList<int> now, int nowindex)
+        {
+            if (target == 0)
+            {
+                result.Add(now);
+                return;
+            }
+            if (target < 0)
+            {
+                return;
+            }
+            for (int i = nowindex; i >= 0; i--) 
+            {
+                now.Add(candidates[i]);
+                retell(candidates, target - candidates[i], result, deepcopy(now),i);
+                now.RemoveAt(now.Count - 1);
+            }
+        }
+    }
+}
---
+++ 2017-9-20/2017-9-20/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_9_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 10, 1, 2, 7, 6, 1, 5 };
+            Solution a = new Solution();
+            a.CombinationSum2(nums, 8);
+
+        }
+    }
+    public class Solution
+    {
+        public IList<IList<int>> CombinationSum2(int[] candidates, int target)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            qSort(candidates, 0, candidates.Length - 1);
+
+
+            return result;
+        }
+        public void reCall(IList<IList<int>> result, int[] candidates, int target, IList<int> now, int indexnow)
+        {
+
+        }
+        public List<int> deepCopy(List<int> now)
+        {
+            List<int> result = new List<int>();
+            for (int i = 0; i < now.Count; i++)
+            {
+                result.Add(now[i]);
+            }
+            return result;
+        }
+        public void qSort(int[] nums, int begin, int end)
+        {
+            if (begin >= end)
+                return;
+            int a = begin;int b = end;
+            while (b > a)  
+            {
+                while (b > a && nums[b] >= nums[a])
+                {
+                    b--;
+                }
+                int temp = nums[b];
+                nums[b] = nums[a];
+                nums[a] = temp;
+                while (b > a && nums[b] >= nums[a])
+                {
+                    a++;
+                }
+                temp = nums[b];
+                nums[b] = nums[a];
+                nums[a] = temp;
+            }
+            qSort(nums, begin, a - 1);
+            qSort(nums, b + 1, end);
+        }
+    }
+}

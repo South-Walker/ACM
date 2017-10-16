2c7f5f33f6ad11eefc212904459795976be456b6
Nanxing Xiao <506830915@qq.com>
2017-10-16 22:37:16 +0800

    2017-10-16

---
+++ 2017-10-15/2017-10-15/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_10_15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    /**
+ * Definition for an interval.
+ */ public class Interval
+    {
+        public int start;
+        public int end;
+        public Interval() { start = 0; end = 0; }
+        public Interval(int s, int e) { start = s; end = e; }
+    }
+ 
+    public class Solution
+    {
+        public IList<Interval> Merge(IList<Interval> intervals)
+        {
+            List<Interval> listintervals = intervals.ToList();
+            listintervals.Sort((x, y) =>
+            {
+                return x.start.CompareTo(y.start);
+            });
+            List<Interval> result = new List<Interval>();
+            if (listintervals.Count == 0)
+                return result;
+            Interval before = listintervals[0];
+            Interval now;
+            result.Add(before);
+            for (int i = 1; i < listintervals.Count; i++)
+            {
+                now = listintervals[i];
+                if (now.start > before.end)
+                {
+                    result.Add(now);
+                }
+                else if (now.end >= before.end) 
+                {
+                    result[result.Count - 1].end = now.end;
+                }
+                before = result[result.Count - 1];
+            }
+            return result;
+        }
+    }
+}
---
+++ 2017-10-16/2017-10-16/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_10_16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Interval> test = new List<Interval>();
+            test.Add(new Interval(1, 5));
+         //   test.Add(new Interval(6, 9));
+            Solution a = new Solution();
+            a.Insert(test, new Interval(6, 7));
+        }
+    }
+    /**
+ * Definition for an interval.
+ */ public class Interval
+    {
+        public int start;
+        public int end;
+        public Interval() { start = 0; end = 0; }
+        public Interval(int s, int e) { start = s; end = e; }
+    }
+ 
+    public class Solution
+    {
+        public IList<Interval> Insert(IList<Interval> intervals, Interval newInterval)
+        {
+            List<Interval> result = new List<Interval>();bool inserting = false;
+
+            if (intervals.Count == 0)
+            {
+                result.Add(newInterval);
+                return result;
+            }
+            if (newInterval.start > intervals[intervals.Count - 1].end)
+            {
+                intervals.Add(newInterval);
+                return intervals;
+            }
+            Interval now = null;
+            for (int i = 0; i < intervals.Count; i++)
+            {
+                now = intervals[i];
+                if (inserting)
+                {
+                    newInterval.start = Math.Min(newInterval.start, now.start);
+                    if (now.start > newInterval.end)
+                    {
+                        result.Add(newInterval);
+                        result.Add(now);
+                        inserting = false;
+                    }
+                    else if (newInterval.end <= now.end)
+                    {
+                        newInterval.end = now.end;
+                        result.Add(newInterval);
+                        inserting = false;
+                    }
+                }
+                else
+                {
+                    if (now.end < newInterval.start || now.start > newInterval.end) 
+                    {
+                        result.Add(now);
+                        continue;
+                    }
+                    else
+                    {
+                        inserting = true;
+                        i--;
+                    }
+                }
+            }
+            if (inserting) 
+                result.Add(newInterval);
+            return result;
+        }
+    }
+}

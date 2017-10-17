541d091dda9a8a656df568dcf3eae1613861fdcd
Nanxing Xiao <506830915@qq.com>
2017-10-17 19:51:46 +0800

    2017-10-17

---
+++ 2017-10-17/2017-10-17/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_10_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution a = new Solution();
+            a.UniquePaths(3, 7);
+        }
+    }
+    public class Solution
+    {
+        public int UniquePaths(int m, int n)
+        {
+            int[,] result = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == 0 && j == 0)
+                    {
+                        result[i, j] = 1;
+                        continue;
+                    }
+
+                    int up = (i == 0) ? 0 : result[i - 1, j];
+                    int left = (j == 0) ? 0 : result[i, j - 1];
+                    result[i, j] = up + left;
+                }
+            }
+            return result[m - 1, n - 1];
+        }
+    }
+}

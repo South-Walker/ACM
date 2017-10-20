acaf6e2091eb73d75bbe2385130b7bfa7153bf5f
Nanxing Xiao <506830915@qq.com>
2017-10-20 18:40:43 +0800

    2017-10-20

---
+++ 2017-10-19/2017-10-19/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_10_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] grid = { { 1, 3, 1 }, { 1, 5, 1 }, { 4, 2, 1 } };
+            Solution a = new Solution();
+            a.MinPathSum(grid);
+        }
+    }
+    public class Solution
+    {
+        public int MinPathSum(int[,] grid)
+        {
+            int[,] dp = new int[grid.GetLength(0), grid.GetLength(1)];
+            for (int i = 0; i < dp.GetLength(0); i++)
+            {
+                for (int k = 0; k < dp.GetLength(1); k++) 
+                {
+                    int up = (i - 1 >= 0) ? dp[i - 1, k] : int.MaxValue;
+                    int left = (k - 1 >= 0) ? dp[i, k - 1] : int.MaxValue;
+                    int min = Math.Min(up, left);
+
+                    dp[i, k] = (min == int.MaxValue) ? grid[0, 0] : min + grid[i, k];
+                }
+            }
+            return dp[dp.GetLength(0) - 1, dp.GetLength(1) - 1];
+        }
+    }
+}
---
+++ 2017-10-20/2017-10-20/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_10_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] test = { { 0, 0, 0, 5 }, { 4, 3, 1, 4 }, { 0, 1, 1, 4 }, { 1, 2, 1, 3 }, { 0, 0, 1, 1 } };
+            Solution a = new Solution();
+            a.SetZeroes(test);
+        }
+    }
+    public class Solution
+    {
+        public void SetZeroes(int[,] matrix)
+        {
+            bool isrowzero = false;
+            bool iscolzero = false;
+            for (int i = 0; i < matrix.GetLength(0); i++) 
+            {
+                if (matrix[i, 0] == 0)
+                {
+                    isrowzero = true;
+                    break;
+                }
+            }
+            for (int i = 0; i < matrix.GetLength(1); i++) 
+            {
+                if (matrix[0, i] == 0)
+                {
+                    iscolzero = true;
+                    break;
+                }
+            }
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 1; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == 0)
+                    {
+                        matrix[0, j] = 0;
+                        matrix[i, 0] = 0;
+                    }
+                }
+            }
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                if (matrix[i, 0] == 0)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                if (matrix[0, j] == 0)
+                {
+                    for (int i = 0; i < matrix.GetLength(0); i++)
+                    {
+                        matrix[i, j] = 0;
+                    }
+                }
+            }
+            if (isrowzero)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    matrix[i, 0] = 0;
+                }
+            }
+            if (iscolzero) 
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[0, j] = 0;
+                }
+            }
+        }
+    }
+}

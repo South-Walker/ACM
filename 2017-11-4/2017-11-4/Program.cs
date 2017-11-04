96e2242edfce8e9ae578172955e9cdea43d3dfe9
Nanxing Xiao <506830915@qq.com>
2017-11-04 14:04:48 +0800

    2017-11-4

---
+++ 2017-11-4/2017-11-4/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_11_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public int LengthOfLastWord(string s)
+        {
+            int len = s.Length;
+            int counter = 0;
+            if (len == 0)
+                return counter;
+            char pre, now;
+            pre = s[0];
+            if (pre != ' ')
+                counter++;
+            for (int i = 1; i < len; i++)
+            {
+                now = s[i];
+                if (pre == ' ')
+                {
+                    if (now == ' ')
+                        ;
+                    else
+                        counter = 1;
+                }
+                else
+                {
+                    if (now == ' ')
+                        ;
+                    else
+                        counter++;
+                }
+                pre = s[i];
+            }
+            return counter;
+        }
+    }
+}

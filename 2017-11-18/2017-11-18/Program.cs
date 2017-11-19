df24dcd35ac932623a2d39838863727e433e8a5b
Nanxing Xiao <506830915@qq.com>
2017-11-19 15:28:12 +0800

    2017-11-19

---
+++ 2017-11-18/2017-11-18/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_11_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] test = { 2, 1,2 };
+            Solution a = new Solution();
+            a.SortColors(test);
+            Console.Read();
+        }
+    }
+    public class Solution
+    {
+        public void SortColors(int[] nums)
+        {
+            int beforeindex = 0;
+            int maxindex = nums.Length - 1;
+            if (maxindex < 1)
+                return;
+            for (int colornow = 0; colornow < 3; colornow++)
+            {
+                int nowindex = beforeindex;
+                while (nowindex <= maxindex)
+                {
+                    if (nums[beforeindex] == colornow)
+                    {
+                        beforeindex++;
+                        while (nowindex <= beforeindex)
+                            nowindex++;
+                        continue;
+                    }
+                    else
+                    {
+                        while (nowindex <= maxindex && nums[nowindex] != colornow)
+                        {
+                            nowindex++;
+                        }
+                        if (nowindex > maxindex)
+                            break;
+                        else
+                        {
+                            swap(nums, nowindex, beforeindex);
+                            nowindex++;
+                        }
+                    }
+                }
+                if (beforeindex < maxindex && nums[beforeindex] == colornow) 
+                    beforeindex++;
+            }
+        }
+        private void swap(int[] nums, int indexa, int indexb)
+        {
+            int temp = nums[indexa];
+            nums[indexa] = nums[indexb];
+            nums[indexb] = temp;
+        }
+    }
+}
---
+++ 2017-11-19/2017-11-19/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_11_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ListNode head = new ListNode(1);
+            head.next = new ListNode(2);
+            Solution a = new Solution();
+            a.Partition(head, 0);
+        }
+    }
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+    public class Solution
+    {
+        public ListNode Partition(ListNode head, int x)
+        {
+            if (head == null || head.next == null)
+                return head;
+            int len = 1; ListNode tail = head;
+            while (tail.next != null)
+            {
+                tail = tail.next;
+                len++;
+            }
+            ListNode now = head;
+            ListNode newhead = new ListNode(int.MinValue);
+            ListNode prenow = newhead;
+            newhead.next = head;
+            head = newhead;
+            while (len != 0)
+            {
+                if (now.val >= x)
+                {
+                    if (now.next == null)
+                        break;
+                    prenow.next = now.next;
+                    tail.next = now;
+                    tail = now;
+                }
+                if (prenow.next != now.next)
+                    prenow = now;
+                now = now.next;
+                tail.next = null;
+                len--;
+            }
+            return head.next;
+        }
+    }
+}

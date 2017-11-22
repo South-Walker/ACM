6ca2bf32aab6876ed6a14bca6ef2a72530066831
Nanxing Xiao <506830915@qq.com>
2017-11-22 10:49:29 +0800

    2017-11-22

---
+++ 2017-11-22/2017-11-22/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_11_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution a = new Solution();
+            a.NumTrees(5);
+        }
+    }
+    public class Solution
+    {
+        static int[] dptable = new int[30];
+        public int NumTrees(int n)
+        {
+            if (n == 0)
+                return n;
+            dptable[1] = 1;
+            dptable[0] = 1;
+            dptable[2] = 2;
+            if (dptable[n] == 0)
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    if (dptable[i] == 0)
+                    {
+                        int now = 0;
+                        for (int k = 0;; k++)
+                        {
+                            if ((i - 1) % 2 == 0 && k == (i - 1) / 2)
+                                break;
+                            if ((i - 1) % 2 != 0 && k == (i + 1) / 2)
+                                break;
+                            now = now + dptable[k] * dptable[i - 1 - k];
+                        }
+                        now *= 2;
+                        if ((i - 1) % 2 == 0)
+                        {
+                            now += dptable[(i - 1) / 2] * dptable[(i - 1) / 2];
+                        }
+                        dptable[i] = now;
+                    } 
+                }
+            }
+            return dptable[n];
+        }
+    }
+}

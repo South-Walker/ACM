5f95b7cf3fee0dd218d36e524b3d4be4c329b17e
Nanxing Xiao <506830915@qq.com>
2017-11-23 20:58:13 +0800

    2017-11-23

---
+++ 2017-11-23/2017-11-23/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_11_23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums.Length <= 2)
+                return nums.Length;
+            int readindex = 0;
+            int writeindex = 0;
+            int dnum = 1;
+            int prenum = int.MaxValue;
+            for (; readindex < nums.Length; readindex++)
+            {
+                if (nums[readindex] == prenum)
+                {
+                    dnum++;
+                }
+                else
+                {
+                    dnum = 1;
+                    prenum = nums[readindex];
+                    nums[writeindex] = nums[readindex];
+                    writeindex++;
+                    continue;
+                }
+                if (dnum <= 2)
+                {
+                    nums[writeindex] = nums[readindex];
+                    writeindex++;
+                    continue;
+                }
+            }
+            return writeindex++;
+        }
+    }
+}

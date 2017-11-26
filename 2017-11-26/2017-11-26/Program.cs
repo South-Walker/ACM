9555c9cda51e88a4b36984fec9aa77b357b37320
Nanxing Xiao <506830915@qq.com>
2017-11-26 15:36:50 +0800

    2017-11-26

---
+++ 2017-11-26/2017-11-26/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2017_11_26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int result = 0;
+            foreach (var item in nums)
+            {
+                result = result ^ item;
+            }
+            return result;
+        }
+    }
+}

aaaf0edb1363a41c0b7dc0889e8c334c9a9948c8
South-Walker <506830915@qq.com>
2018-04-02 18:54:33 +0800

    2018-4-2

---
+++ 2018-4-2/2018-4-2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2018_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 10, 9, 2, 5, 3, 7, 101, 18 };
+            Solution a = new Solution();
+            a.LengthOfLIS(nums);
+        }
+    }
+    public class Solution
+    {
+        public int slowLengthOfLIS(int[] nums)
+        {
+            if (nums.Length == 0)
+                return 0;
+            int[] m = new int[nums.Length];
+            int[] d = new int[nums.Length];
+            m[0] = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                m[i] = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[j] < nums[i])
+                    {
+                        m[i] = Math.Max(m[i], m[j] + 1);
+                    }
+                }
+            }
+            int max = int.MinValue;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                max = Math.Max(max, m[i]);
+            }
+            return max;
+        }
+        public int LengthOfLIS(int[] nums)
+        {
+
+            return 0;
+        }
+    }
+}

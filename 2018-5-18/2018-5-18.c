228cfa62176e7d543b9ded92dab42063d1ee0df7
South-Walker <506830915@qq.com>
2018-05-19 14:55:21 +0800

    2018-5-19

--- 2018-5-18/2018-5-18.c
+++ 2018-5-18/2018-5-18.c
@@ -3,46 +3,175 @@
  */
 #include <stdio.h>
 #include <malloc.h>
+#include <limits.h>
 int* twoSum(int*,int,int);
 int* twoSumWithoutSort(int*,int,int);
+void qSort(int*,int,int);
 void Swap(int*,int*);
+int** fourSum(int*,int,int,int*);
+int** threeSum(int*,int,int*);
 
 main()
 {
-	int nums[] = {2,7,11,15};
+	int nums[] = {-1,-5,-5,-3,2,5,0,4};
 	int length = sizeof(nums)/sizeof(nums[0]);
-	int i;
-	int *answer = twoSum(nums,length,9);
+	int i,j;
+	int *returnSize = (int *)malloc(sizeof(int)); 
+	int **answers = fourSum(nums,length,-7,returnSize);
 	for(i=0;i<length;i++)
 	{
 		printf("%d ",nums[i]);
 	}
-	printf("\n%d %d",*answer,*(answer+1));
+	printf("\n%d",*returnSize);
+	for(i=0;i<*returnSize;i++)
+	{
+		int*now = answers[i];
+		printf("\n%5d %5d %5d %5d",now[0],now[1],now[2],now[3]);
+	}
 }
-int** threeSum(int* nums, int numsSize, int* returnSize) {
-    
+int** fourSum(int* nums, int numsSize, int target, int* returnSize)
+{
+    qSort(nums,0,numsSize-1);
+    int i,j,k,l;
+    int lasti=INT_MAX,lastj=INT_MAX,lastk=INT_MAX;
+    int nowvalue;
+    int **result, *oneanswer;
+    int count=0,pointLen=sizeof(int*),quaLen=4*sizeof(int);
+    for(i=0;i<numsSize-3;i++)
+    {
+    	if(nums[i]==lasti)
+    		continue;
+    	lastj=INT_MAX,lastk=INT_MAX;
+    	for(j=i+1;j<numsSize-2;j++)
+    	{
+    		if(nums[j]==lastj)
+    			continue;
+    		lastk=INT_MAX;
+    		k=j+1,l=numsSize-1;
+    		while(k!=l)
+    		{
+    			nowvalue = nums[i]+nums[j]+nums[k]+nums[l];
+    			if(nowvalue>target)
+    			{
+    				l--;
+				}
+				else if(nowvalue<target)
+				{
+					k++;
+				}
+				else
+				{
+					if(nums[k]==lastk)
+					{
+						k++;
+						continue;
+					}
+					oneanswer = (int *)malloc(quaLen);
+					oneanswer[0] = nums[i];
+					oneanswer[1] = nums[j];
+					oneanswer[2] = nums[k];
+					oneanswer[3] = nums[l];
+					if(count)
+					{
+						result = (int **)realloc(result,(count+1)*pointLen);
+					}
+					else
+					{
+						result = (int **)malloc(pointLen);
+					}
+					result[count] = oneanswer;
+					lasti = nums[i];
+					lastj = nums[j];
+					lastk = nums[k];
+					count++;
+					k++;
+				}
+			}
+		}
+	}
+	*returnSize = count;
+	return result;
+}
+int** threeSum(int* nums, int numsSize, int* returnSize) 
+{
+	int count = 0;
+	int triLen = 3*sizeof(int);
+	int pointLen = sizeof(int*);
+	int **result,*oneanswer;
+	qSort(nums,0,numsSize-1);
+	int i,j,k,nowvalue,lasti=1,lastj=INT_MAX;
+	for(i=0;i<numsSize - 2;i++)
+	{
+		if(nums[i]==lasti)
+			continue;
+		else
+			lasti=nums[i];
+		j = i+1;
+		k = numsSize-1;
+		while(j!=k)
+		{
+			nowvalue =  nums[i]+nums[j]+nums[k];
+			if(nowvalue>0)
+			{
+				k--;
+			}
+			else if(nowvalue<0)
+			{
+				j++;
+			}
+			else
+			{
+				if(nums[j]==lastj)
+				{
+					j++;
+					continue;
+				}
+				lastj = nums[j];
+				oneanswer = (int *)malloc(triLen);
+				oneanswer[0] = nums[i];
+				oneanswer[1] = nums[j];
+				oneanswer[2] = nums[k];
+				if(count==0)
+				{
+					result = (int **)malloc(pointLen);
+				}
+				else
+				{
+					result = (int **)realloc(result,(count+1)*pointLen);
+				}
+				result[count]=oneanswer;
+				count++;
+				j++;
+			}
+		}
+		lastj=INT_MAX;
+	}
+	*returnSize = count;
+	return result;
 }
 void qSort(int* nums,int beginindex,int endindex)
 {
+	if(endindex<=beginindex)
+		return;
 	int nowbegin = beginindex,nowend = endindex;
 	while(nowend>nowbegin)
 	{
-		while(nums[nowbegin]<nums[nowend] && nowend>nowbegin)
+		while(nums[nowbegin]<=nums[nowend] && nowend>nowbegin)
 		{
 			nowbegin++;
 		}
 		Swap(nums+nowbegin,nums+nowend); 
-		while(nums[nowbegin]<nums[nowend] && nowend>nowbegin)
+		while(nums[nowbegin]<=nums[nowend] && nowend>nowbegin)
 		{
 			nowend--;
 		}
-		Swap(nums+nowbegin,nums+nowend); 
+		Swap(nums+nowbegin,nums+nowend);
 	}
-	if(nowbegin-1>beginindex)
+	if(nowbegin-1<endindex)
 	{
 		qSort(nums,beginindex,nowbegin-1);
 	}
-	if(nowend+1<endindex)
+	if(nowend+1>beginindex)
 	{
 		qSort(nums,nowend+1,endindex);
 	}

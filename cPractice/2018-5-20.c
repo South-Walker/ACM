6ee62192c617be7c30e356c392acf4e7f338d4cf
South-Walker <506830915@qq.com>
2018-05-20 23:47:04 +0800

    2018-5-20

--- cPractice/2018-5-20.c
+++ cPractice/2018-5-20.c
@@ -1,9 +1,57 @@
 #include <stdio.h>
+#include <malloc.h>
+#include <limits.h>
 main()
 {
-	int height[]={2,3,4,5,18,17,6};
-	int len=sizeof(height)/sizeof(int);
-	printf("%d", maxArea(height,len));
+	int *dungeon[3],i=0;
+	int now1[]={-2,-3,3};
+	int now2[]={-5,-10,1};
+	int now3[]={10,30,-5};
+	dungeon[0]=now1;
+	dungeon[1]=now2;
+	dungeon[2]=now3;
+	int rowlen=3;
+	int collens[]={3,3,3};
+	printf("%d", calculateMinimumHP(dungeon,rowlen,collens));
+}
+int calculateMinimumHP(int** dungeon, int dungeonRowSize, int *dungeonColSizes)
+{
+	int *dp[dungeonRowSize];
+	int i,j;
+	int nextMinHP=INT_MAX;
+	for(i=0;i<dungeonRowSize;i++)
+	{
+		dp[i]=(int*)malloc(dungeonColSizes[i]*sizeof(int));
+	}
+	for(i=dungeonRowSize-1;i>=0;i--)
+	{
+		for(j=dungeonColSizes[i]-1;j>=0;j--)
+		{
+			nextMinHP=INT_MAX;
+			if(j==dungeonColSizes[i]-1 && i==dungeonRowSize-1)
+				nextMinHP=0;
+			else
+			{
+				if(j<dungeonColSizes[i]-1)
+				{
+					nextMinHP=(nextMinHP>dp[i][j+1])?dp[i][j+1]:nextMinHP;
+				}
+				if(i<dungeonRowSize-1)
+				{
+					nextMinHP=(nextMinHP>dp[i+1][j])?dp[i+1][j]:nextMinHP;
+				}
+			}
+			if(dungeon[i][j]<0)
+			{
+				dp[i][j]=nextMinHP-dungeon[i][j];
+			}
+			else
+			{
+				dp[i][j]=(0>nextMinHP-dungeon[i][j])?0:nextMinHP-dungeon[i][j]; 
+			}
+		}
+	}
+	return dp[0][0]+1;    
 }
 int maxArea(int* height, int heightSize) 
 {
@@ -15,21 +63,23 @@ int maxArea(int* height, int heightSize)
     x*=(heightSize-1);
     int *PFronter=height,*PLatter=height+heightSize-1;
     int temp;
-    while(PFronter!=PLatter)
+    while(PFronter<PLatter)
     {
-    	temp=*PFronter;
-    	while(*PFronter<=temp&&PFronter!=PLatter)
+    	if(*PFronter<=*PLatter)
     	{
-    		PFronter++;
+    		temp=*PFronter;
+    		while(*PFronter<=temp)
+    		{
+  	  			PFronter++;
+			}
 		}
-		temp=(*PFronter>=*PLatter)?*PLatter:*PFronter;
-		temp*=(PLatter-PFronter);
-		x=(temp>x)?temp:x;
-		
-    	temp=*PLatter;
-    	while(*PLatter<=temp&&PFronter!=PLatter)
-    	{
-    		PLatter--;
+		else
+		{
+    		temp=*PLatter;
+    		while(*PLatter<=temp)
+    		{
+    			PLatter--;
+			}
 		}
 		temp=(*PFronter>=*PLatter)?*PLatter:*PFronter;
 		temp*=(PLatter-PFronter);

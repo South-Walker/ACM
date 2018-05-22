69d10ddbed91b8af1eabbf24be64babeaabc3d56
South-Walker <506830915@qq.com>
2018-05-22 22:47:27 +0800

    2018-5-22

---
+++ cPractice/DataStructures/LinearTable.c
@@ -0,0 +1,88 @@
+#include <stdio.h>
+#include <malloc.h>
+typedef int ElemType;
+#define INITSIZE 100
+typedef struct
+{
+	ElemType *data;
+	int length;
+	int listsize;
+}sqlist;
+void initlist(sqlist *L)
+{
+	L->data=(ElemType*)malloc(sizeof(ElemType)*INITSIZE);
+	L->length=0;
+	L->listsize=INITSIZE;
+}
+int getlen(sqlist *L)
+{
+	return (L->length);
+}
+int getelem(sqlist *L,int i,ElemType *e)
+{
+	if(i<1||i>L->length)
+		return 0;
+	*e=L->data[i-1];
+	return 1;
+}
+int locate(sqlist *L,ElemType x)
+{
+	int i=0;
+	while(i<L->length)
+	{
+		if(L->data[i]==x)
+			return i+1;
+		else
+			i++;
+	}
+	return 0;
+}
+int insert(sqlist*L,int i,ElemType x)
+{
+	int j;
+	ElemType *data=L->data;
+	int length=L->length;
+	if(i<1||i>L->length+1)
+		return 0;
+	if(L->length==L->listsize)
+	{
+		data=(ElemType*)realloc(data,(L->listsize+1)*sizeof(ElemType));
+		L->listsize++;
+		L->data=data;
+	}
+	for(j=L->length-1;j>=i-1;j--)
+	{
+		data[j+1]=data[j];
+	}
+	data[i-1]=x;
+	L->length++;
+	return 1;
+}
+int delete(sqlist*L,int i,ElemType *e)
+{
+	int j;
+	ElemType *data=L->data;
+	int length=L->length;
+	if(i<1||i>length+1)
+		return 0;
+	*e=data[i-1];
+	for(j=i-1;j<length;j++)
+	{
+		data[j]=data[j+1];
+	}
+	L->length--;
+	return 1;
+}
+void list(sqlist*L)
+{
+	int i,length=L->length;
+	ElemType *data=L->data;
+	for(i=0;i<length;i++)
+	{
+		printf("%5d",data[i]);
+	}
+	printf("\n");
+}
+main()
+{
+}

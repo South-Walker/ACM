0ab376ba07ac7015c7584ee91a8f2c8691933c63
South-Walker <506830915@qq.com>
2018-05-23 18:51:30 +0800

    2018-5-23

--- cPractice/2018-5-22.c
+++ cPractice/2018-5-22.c
@@ -1,88 +1,117 @@
 #include <stdio.h>
 #include <malloc.h>
-typedef int ElemType;
-#define INITSIZE 100
-typedef struct
+struct ListNode 
 {
-	ElemType *data;
-	int length;
-	int listsize;
-}sqlist;
-void initlist(sqlist *L)
+    int val;
+    struct ListNode *next;
+};
+
+list(struct ListNode* head)
 {
-	L->data=(ElemType*)malloc(sizeof(ElemType)*INITSIZE);
-	L->length=0;
-	L->listsize=INITSIZE;
-}
-int getlen(sqlist *L)
-{
-	return (L->length);
-}
-int getelem(sqlist *L,int i,ElemType *e)
-{
-	if(i<1||i>L->length)
-		return 0;
-	*e=L->data[i-1];
-	return 1;
-}
-int locate(sqlist *L,ElemType x)
-{
-	int i=0;
-	while(i<L->length)
+	while(head!=NULL)
 	{
-		if(L->data[i]==x)
-			return i+1;
-		else
-			i++;
-	}
-	return 0;
-}
-int insert(sqlist*L,int i,ElemType x)
-{
-	int j;
-	ElemType *data=L->data;
-	int length=L->length;
-	if(i<1||i>L->length+1)
-		return 0;
-	if(L->length==L->listsize)
-	{
-		data=(ElemType*)realloc(data,(L->listsize+1)*sizeof(ElemType));
-		L->listsize++;
-		L->data=data;
-	}
-	for(j=L->length-1;j>=i-1;j--)
-	{
-		data[j+1]=data[j];
-	}
-	data[i-1]=x;
-	L->length++;
-	return 1;
-}
-int delete(sqlist*L,int i,ElemType *e)
-{
-	int j;
-	ElemType *data=L->data;
-	int length=L->length;
-	if(i<1||i>length+1)
-		return 0;
-	*e=data[i-1];
-	for(j=i-1;j<length;j++)
-	{
-		data[j]=data[j+1];
-	}
-	L->length--;
-	return 1;
-}
-void list(sqlist*L)
-{
-	int i,length=L->length;
-	ElemType *data=L->data;
-	for(i=0;i<length;i++)
-	{
-		printf("%5d",data[i]);
+		printf("%5d",head->val);
+		head=head->next;
 	}
 	printf("\n");
 }
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) 
+{
+    if(l1==NULL)
+    	return l2;
+    if(l2==NULL)
+    	return l1;
+    int flag=0;
+    struct ListNode* result=l1;
+    while(l1->next!=NULL&&l2->next!=NULL)
+    {
+    	l1->val+=l2->val+flag;
+    	flag=0;
+    	if(l1->val>=10)
+    	{
+    		l1->val-=10;
+    		flag=1;
+		}
+		l1=l1->next;
+		l2=l2->next;
+	}
+    l1->val+=l2->val+flag;
+    flag=0;
+    if(l1->val>=10)
+    {
+    	l1->val-=10;
+    	flag=1;
+	}
+	//保证l1最长 
+	if(l1->next==NULL)
+	{
+		l1->next=l2->next;
+	}
+	while(l1->next!=NULL&&flag!=0)
+	{
+		l1=l1->next;
+		l1->val+=flag;
+		if(l1->val>=10)
+		{
+			l1->val-=10;
+		}
+		else
+		{
+			flag=0;
+		}
+	}
+	if(flag)
+	{
+		l1->next=(struct ListNode*)malloc(sizeof(struct ListNode));
+		l1=l1->next;
+		l1->val=1;
+		l1->next=NULL;
+	}
+	return result;
+}
 main()
 {
+	int a1[]={5};
+	int a2[]={5};
+	int i;
+	int len1=sizeof(a1)/sizeof(int);
+	int len2=sizeof(a2)/sizeof(int);
+	struct ListNode *l1;
+	struct ListNode *l2;
+	
+	struct ListNode *head;
+	head=(struct ListNode *)malloc(sizeof(struct ListNode));
+	head->next=NULL;
+	l1=head;
+	struct ListNode *temp=(struct ListNode *)malloc(sizeof(struct ListNode));
+	for(i=0;i<len1;i++)
+	{
+		temp->val=a1[i];
+		temp->next=NULL;
+		head->next=temp;
+		head=temp;
+		temp=(struct ListNode *)malloc(sizeof(struct ListNode));
+	}
+	head=l1;
+	l1=l1->next;
+	
+	temp=(struct ListNode *)malloc(sizeof(struct ListNode));
+	head->next=NULL;
+	l2=head;
+	for(i=0;i<len2;i++)
+	{
+		temp->val=a2[i];
+		temp->next=NULL;
+		head->next=temp;
+		head=temp;
+		temp=(struct ListNode *)malloc(sizeof(struct ListNode));
+	}
+	head=l2;
+	l2=l2->next;
+	free(head);
+	list(l1);
+	list(l2);
+	
+	struct ListNode *result=addTwoNumbers(l1,l2);
+	list(result);
 }
--- cPractice/DataStructures/Link.c
+++ cPractice/DataStructures/Link.c
@@ -33,12 +33,106 @@ int getlen(slink *head)
 }
 int getelem(slink *head,int i,ElemType *e)
 {
-	
+	if(i<=0)
+		return 0; 
+	int j;
+	for(j=0;j<i;j++)
+	{
+		head=head->next;
+		if(head==NULL)
+		{
+			return 0;
+		}
+	}
+	*e=head->data;
+	return 1;
+}
+int locate(slink *head,ElemType e)
+{
+	int i=0;
+	head=head->next;
+	while(head!=NULL)
+	{
+		i++;
+		if(head->data==e)
+		{
+			return i;
+		}
+		head=head->next;
+	}
+	return 0;
+} 
+int delete(slink *head,int i,ElemType *e)
+{
+	if(i<1)
+		return 0; 
+	slink *prenode=head,*frontnode=head->next;
+	//frontnodeµÄÏÂ±ê 
+	int j=1;
+	while(j!=i)
+	{
+		if(frontnode==NULL)
+			return 0;
+		else
+		{
+			j++;
+			prenode=frontnode;
+			frontnode=frontnode->next;
+		}
+	}
+	if(frontnode==NULL)
+		return 0;
+	prenode->next=frontnode->next;
+	*e=frontnode->data;
+	free(frontnode); 
+	return 1; 
+}
+int insert(slink *head,int i,ElemType x)
+{
+	slink *p,*q;
+	int j;
+	if(i<1)
+		return 0;
+	p=head;j=0;
+	while(p!=NULL&&j<i-1)
+	{
+		p=p->next;
+		j++;
+	}
+	if(p==NULL)
+		return 0;
+	q=(slink *)malloc(sizeof(slink));
+	q->data=x;
+	q->next=p->next;
+	p->next=q;
+	return 1;
+}
+list(slink *head)
+{
+	head=head->next;
+	while(head!=NULL)
+	{
+		printf("%5d",head->data);
+		head=head->next;
+	}
+	printf("\n");
 }
 main()
 {
 	slink *link=creslink(50);
-	printf("%5d",getlen(link));
-	printf("%5d",getlen(link));
-	printf("%5d",link->next->data);
+	int *e=(int *)malloc(sizeof(int));
+	int r;
+	r=delete(link,51,e);
+	printf("%5d%5d\n",r,*e);
+	r=delete(link,50,e);
+	printf("%5d%5d\n",r,*e);
+	r=delete(link,50,e);
+	printf("%5d%5d\n",r,*e);
+	list(link); 
+	for(r=49;r>0;r--)
+	{
+		delete(link,r,e);
+		insert(link,r,-(r-1));
+	}
+	list(link);
 }

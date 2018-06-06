749e37221cccad20712c3bce912cadd9fb8973a9
South-Walker <506830915@qq.com>
2018-06-06 15:19:29 +0800

    2018-6-6

---
+++ cPractice/2018-6-6.c
@@ -0,0 +1,143 @@
+#include <stdio.h>
+#include <malloc.h> 
+typedef struct node
+{
+	long long data;
+	struct node* lchild;
+	struct node* rchild;
+}bst;
+void freebst(bst*root)
+{
+	if(root!=NULL)
+	{
+		freebst(root->lchild);
+		freebst(root->rchild);
+		free(root);
+	}
+}
+void inorder(bst*root)
+{
+	if(root!=NULL)
+	{
+		inorder(root->lchild);
+		printf("%lld ",root->data);
+		inorder(root->rchild);
+	}
+}
+//无重复是1，有重复是0 
+int addtobst(bst*root,long long d)
+{
+	if(root->data==0)
+	{
+		root->data=d;
+		return 1;
+	}
+	bst* pre;
+	bst* worknode=root;
+	do
+	{
+		pre=worknode;
+		if(worknode->data==d)
+		{
+			return 0;
+		}
+		else if(worknode->data>d)
+		{
+			worknode=worknode->lchild;
+		}
+		else
+		{
+			worknode=worknode->rchild;
+		}
+	}while(worknode!=NULL);
+	worknode=(bst*)malloc(sizeof(bst));
+	worknode->data=d;
+	worknode->lchild=NULL;
+	worknode->rchild=NULL;
+	if(pre->data<d)
+	{
+		pre->rchild=worknode;
+	}
+	else
+	{
+		pre->lchild=worknode;
+	}
+	return 1;
+}
+int uniqueMorseRepresentations(char** words, int wordsSize) 
+{
+	
+    int i,j,k;
+    char* code;
+    char *codes[26]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+    bst*root=(bst*)malloc(sizeof(bst));
+    root->data=0;
+    root->lchild=NULL;
+    root->rchild=NULL;
+    int count=0;
+    long long value;
+    for(i=0;i<wordsSize;i++)
+    {
+    	value=1;
+    	j=0;
+    	while(words[i][j]!='\0')
+    	{
+    		code=codes[words[i][j]-'a'];
+    		k=0;
+    		while(code[k]!='\0')
+    		{
+    			if(code[k]=='-')
+    			{
+    				value*=2;
+				}
+				else
+				{
+					value=value*2+1; 
+				}
+				k++;
+			}
+    		j++;
+		}
+		count+=addtobst(root,value);
+	}
+	freebst(root);
+	return count;
+}
+int* numberOfLines(int* widths, int widthsSize, char* S, int* returnSize) 
+{
+    int row=1;
+	int count=0;
+	int sindex=0;
+	while(S[sindex]!='\0')
+	{
+		if(
+		100< (count+=(widths[S[sindex]-'a']))
+		)
+		{
+			count=widths[S[sindex]-'a'];
+			row++;
+		}
+		sindex++;
+	}
+	*returnSize=2;
+	int* r=(int*)malloc(sizeof(int)*2);
+	r[0]=row;
+	r[1]=count;
+	return r;
+}
+int maxIncreaseKeepingSkyline(int** grid, int gridRowSize, int *gridColSizes) 
+{
+	int count=0;
+	int i,j;
+	for(i=0;i<gridRowSize;i++)
+	{
+		
+	}
+	return count;   
+}
+void main()
+{
+	int i,j;
+	printf("\n%d\n",uniqueMorseRepresentations(ss,len));
+
+}

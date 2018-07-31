9d78e7a6d1e86210ac3e451ceb2c7ff466ee6eeb
South-Walker <506830915@qq.com>
2018-07-31 23:09:19 +0800

    2018-7-31忙里偷闲
    OpenGL环境与VS下注意事项

---
+++ cPractice/2018-7-19.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <malloc.h>
+void main()
+{
+	
+}
+void eoj1029()
+{
+	int n,m;
+	int dp[12][12];
+	
+	while(1)
+	{
+		scanf("%d%d",&n,&m);
+		if(n==0&&m==0)
+		{
+			break;
+		}
+		
+	}
+}
